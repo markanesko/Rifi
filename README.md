cf3f60fe9f5725fae7f09feba9ef4d5d11a2b19a
Aayush9029 <aayushpokharel36@gmail.com>
2020-02-25 14:20:47 -0500

    added watchApp and PythonApp and updated the repo

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-**RIFI v0.02**
+## **RIFI v0.02**
 This is the official repo for Wireless Multimedia controller App/Program "RIFI"
 
 Tested on : MacOS | Windows 10 | Manjaro *Linux*
 
 ---
 
-##  [![Watch OS version](https://img.shields.io/badge/WatchOS-6.1-skyblue?style=flat)](https://www.apple.com/ca/watchos/watchos-6/) [![python 3.8](https://img.shields.io/badge/Python-3.8.1-brightred?style=flat)](https://www.python.org/) [![ask me why](https://img.shields.io/badge/Rifi-v0.02-purple?style=flat)](http://aayush.wtf)
+##  [![Watch OS version](https://img.shields.io/badge/WatchOS-6.1-skyblue?style=flat)](https://www.apple.com/ca/watchos/watchos-6/)   [![python 3.8](https://img.shields.io/badge/Python-3.8.1-brightred?style=flat)](https://www.python.org/)   [![ask me why](https://img.shields.io/badge/Rifi-v0.02-purple?style=flat)](http://aayush.wtf)
 
 ![controlls](Images/controlls.png)Multimedia Controller
 
 
-
 IP Input field.![ip](/Users/aayushpokharel/Desktop/RC/Rifi/Images/ip.png)
-
 
 
 > Installation:
@@ -29,3 +27,42 @@ IP Input field.![ip](/Users/aayushpokharel/Desktop/RC/Rifi/Images/ip.png)
 >
 > *Open App > input ip of the computer > Save > Scroll to multimedia.*
 
+
+
+---
+
+### So how does this work?
+
+- Python stars a local server *using flask (library)* 
+  - Port : 8000 (configurable)
+  - ip : Local host ip (eg: 192.168.1.4)
+- Listens for Inputs (Play/pause, volume up...)
+- Performs the keystrokes in the Laptop that is running flask.
+
+
+
+Basically this transforms an Apple Watch to a virtual remote enabling it to controll multimedia.
+
+Uses:
+
+- While Playing a music in laptop (Play/Pause) (Skip) (Volume up/down) 
+
+- While Watching Movie and keyboard/mouse is a bit too far to reach.
+
+- While playing music to skip tracks and since the keystrokes are configurable they can be use to initiate custom shortcuts.
+
+---
+
+To Do:
+
+- Refactor and Document the code.
+
+- Add a barcode scanner for easy-login. *Instead of inputting the ip scan a barcode?*
+
+- Package the python and iOS application.
+
+  
+
+*Thanks a lot for all the feedback and suggestions, Feel free to raise an issue, submit feature request.*
+
+### [My website](https://aayush.wtf)
---
+++ watchApp/Remote Controller WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,146 @@
+//
+//  InterfaceController.swift
+//  Remote Controller WatchKit Extension
+//
+//  Created by Aayush Pokharel on 2020-02-24.
+//  Copyright © 2020 Aayush Pokharel. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+var url = ""
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var IpPortLabel: WKInterfaceLabel!
+    
+    @IBAction func btn1() {
+        url += "1"
+        IpPortLabel.setText(url)
+    }
+
+    @IBAction func btn2() {
+        url += "2"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btn3() {
+        url += "3"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btn4() {
+        url += "4"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btn5() {
+        url += "5"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btn6() {
+        url += "6"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btn7() {
+        url += "7"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btn8() {
+        url += "8"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btn9() {
+        url += "9"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btnColon() {
+        url += ":"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btnPeriod() {
+        url += "."
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btn0() {
+        url += "0"
+        IpPortLabel.setText(url)
+    }
+    
+    @IBAction func btnClear() {
+        url = ""
+        IpPortLabel.setText("")
+    }
+    var newUrl = ""
+    @IBAction func changeIP() {
+        newUrl = "http://"+url
+        url = newUrl
+        url += "/?key="
+        newUrl = url
+
+        print(url)
+    }
+
+    func PostValue(key:String){
+        url = newUrl
+        url += key
+    
+        let getURL = URL(string: url)!
+            let urlSession = URLSession.shared
+            let getRequest = URLRequest(url: getURL)
+
+            let task = urlSession.dataTask(with: getRequest as URLRequest, completionHandler: { data, response, error in
+                guard error == nil else {
+                    return
+                }
+            })
+            task.resume()
+    }
+    
+    @IBAction func forwardButton() {
+        PostValue(key: "right")
+    }
+    
+    @IBAction func backButton() {
+        PostValue(key: "left")
+    }
+    
+    @IBAction func playPauseButton() {
+        PostValue(key: "space")
+    }
+    
+    @IBAction func volumeUp() {
+        PostValue(key: "up")
+    }
+    
+    @IBAction func volumeDown() {
+        PostValue(key: "down")
+    }
+    
+    @IBAction func homeButton() {
+        PostValue(key: "enter")
+    }
+    
+    var initialValue  = 0.0
+    @IBAction func volumeSlider(_ value: Double) {
+        if (value < initialValue){
+            PostValue(key: "down")
+            initialValue = value
+        }
+        if (value > initialValue){
+            PostValue(key: "up")
+            initialValue = value
+        }
+    }
+    
+
+
+}
